3dc6f7ee66bf129d7fb1da0cdc882b36f1bfd447
Mariia Antonova <mariia_antonova@epam.com>
2022-05-04 00:38:36 +0300

    redundant exceptions deleted

--- src/main/java/com/cynteka/exceptions/ListDividerException.java
+++ src/main/java/com/cynteka/exceptions/ListDividerException.java
@@ -7,16 +7,6 @@ public class ListDividerException extends RuntimeException{
         this.message = message;
     }
 
-    public ListDividerException(String message, String message1) {
-        super(message);
-        this.message = message1;
-    }
-
-    public ListDividerException(String message, Throwable cause, String message1) {
-        super(message, cause);
-        this.message = message1;
-    }
-
     public ListDividerException(Throwable cause, String message) {
         super(cause);
         this.message = message;
