5b45713910003b4c3c821593990ce71ec1aa24d4
Mariia Antonova <mariia_antonova@epam.com>
2022-05-04 18:57:13 +0300

    refactor Main

--- src/main/java/com/cynteka/Main.java
+++ src/main/java/com/cynteka/Main.java
@@ -10,15 +10,21 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        if (args.length < 2) {
-            log.info("Empty args array");
+        if (args == null || args.length < 2) {
+            log.info("Empty or wrong size of args array");
         } else {
             String inputPath = args[0];
             String outputPath = args[1];
 
             List<String> list = TxtFileReader.readNotBlankLinesFromFile(inputPath);
-            List<List<String>> twoSetsOfStrings = ListDivider.divide(list);
-            Map<String, String> map = SimilarStringsMatcher.convert(twoSetsOfStrings);
+            int headListSize = Integer.parseInt(list.get(0));
+            int tailListSize = Integer.parseInt(list.get(headListSize + 1));
+            List<String> head = list.subList(1, headListSize + 1);
+            List<String> tail = list.subList(headListSize + 2, list.size());
+            if (!(head.size() == headListSize && tail.size() == tailListSize)) {
+                log.info("There is mismatch of lengths from file with sizes of result arrays");
+            }
+            Map<String, String> map = SimilarStringsMatcher.convert(head, tail);
             TxtFileWriter.writeToFile(outputPath, map);
         }
     }
