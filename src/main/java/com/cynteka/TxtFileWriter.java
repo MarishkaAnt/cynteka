1998a7a1b52f2404d368086310a0fbcbe736e3a2
Mariia Antonova <mariia_antonova@epam.com>
2022-05-05 11:43:35 +0300

    logger added

--- src/main/java/com/cynteka/TxtFileWriter.java
+++ src/main/java/com/cynteka/TxtFileWriter.java
@@ -1,5 +1,7 @@
 package com.cynteka;
 
+import org.slf4j.Logger;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,18 +12,24 @@ import static java.nio.file.Files.writeString;
 
 public class TxtFileWriter {
 
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(TxtFileWriter.class);
+
     public static void writeToTxTFile(String path, Map<String, String> map) throws IOException {
         if (Files.notExists(Path.of(path))) {
             createFile(Path.of(path));
         }
-        StringBuilder sb = new StringBuilder();
-        map.forEach((key, value) -> {
-            sb.append(key);
-            sb.append(':');
-            sb.append(value);
-            sb.append(System.lineSeparator());
-            sb.append(System.lineSeparator());
-        });
-        writeString(Path.of(path), sb);
+        if (map != null) {
+            StringBuilder sb = new StringBuilder();
+            map.forEach((key, value) -> {
+                sb.append(key);
+                sb.append(':');
+                sb.append(value);
+                sb.append(System.lineSeparator());
+                sb.append(System.lineSeparator());
+            });
+            writeString(Path.of(path), sb);
+        } else {
+            log.info("null input in method writeToTxTFile");
+        }
     }
 }
