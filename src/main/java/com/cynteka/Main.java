ba2153b4b4f704573350ffb031cadbb6edb69626
Mariia Antonova <mariia_antonova@epam.com>
2022-05-05 11:47:24 +0300

    refactor logic of main method

--- src/main/java/com/cynteka/Main.java
+++ src/main/java/com/cynteka/Main.java
@@ -1,31 +1,28 @@
 package com.cynteka;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-@Slf4j
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
         if (args == null || args.length < 2) {
             log.info("Empty or wrong size of args array");
         } else {
             String inputPath = args[0];
             String outputPath = args[1];
-
-            List<String> list = TxtFileReader.readNotBlankLinesFromFile(inputPath);
-            int headListSize = Integer.parseInt(list.get(0));
-            int tailListSize = Integer.parseInt(list.get(headListSize + 1));
-            List<String> head = list.subList(1, headListSize + 1);
-            List<String> tail = list.subList(headListSize + 2, list.size());
-            if (!(head.size() == headListSize && tail.size() == tailListSize)) {
-                log.info("There is mismatch of lengths from file with sizes of result arrays");
+            try {
+                List<String> list = TxtFileReader.readNotBlankLinesFromFile(inputPath);
+                Map<String, String> similarStrings = SimilarStringsMatcher.findSimilarStrings(list);
+                TxtFileWriter.writeToTxTFile(outputPath, similarStrings);
+            } catch (IOException e) {
+                log.error("Something goes wrong: ", e);
             }
-            Map<String, String> map = SimilarStringsMatcher.convert(head, tail);
-            TxtFileWriter.writeToTxTFile(outputPath, map);
             log.info("Success!");
         }
     }
--- src/main/java/com/cynteka/SimilarStringsMatcher.java
+++ src/main/java/com/cynteka/SimilarStringsMatcher.java
@@ -1,6 +1,7 @@
 package com.cynteka;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
 
 import java.util.*;
 
@@ -12,38 +13,58 @@ import java.util.*;
  * output: map with keys of first list strings and values of
  * the same strings founded from second list
  * if same string wasn't found, value = ?
- * <p>
  * Example input:
  * firstList = {Бетон с присадкой}, secondList = {присадка бля бетона, доставка}
  * Example output: map = {присадка бля бетона=Бетон с присадкой, доставка=?}
  */
 public class SimilarStringsMatcher {
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(SimilarStringsMatcher.class);
+
     private static final Map<String, String> synonyms = Map.of(
             "бетон", "цемент",
             "цемент", "бетон",
             "ведро", "корыто",
             "корыто", "ведро");
 
-    public static Map<String, String> findSimilarStrings(List<String> head, List<String> tail) {
-        List<String> firstList = new ArrayList<>(head);
-        List<String> secondList = new ArrayList<>(tail);
-
-        Map<String, String> map = new LinkedHashMap<>();
-        for (String string : firstList) {
-            String result = secondList.stream()
-                    .filter(s -> StringUtils.containsIgnoreCase(s, string)
-                            || StringUtils.containsIgnoreCase(string, s)
-                            || hasSynonymWords(string, s)
-                            || hasSameSubstringWithThisLength(s, string))
-                    .findAny()
-                    .orElse("?");
-            map.put(string, result);
-            secondList.remove(result);
+    public static Map<String, String> findSimilarStrings(List<String> inputList) {
+        int headListSize = 0;
+        int tailListSize = 0;
+        try {
+            headListSize = Integer.parseInt(inputList.get(0));
+            tailListSize = Integer.parseInt(inputList.get(headListSize + 1));
+        } catch (NumberFormatException e) {
+            log.error("There is a mistake in sizes of sets in input.txt ", e);
         }
-        for (String s : secondList) {
-            map.put(s, "?");
+        if (headListSize > 0 || tailListSize > 0) {
+            List<String> firstList = inputList.subList(1, headListSize + 1);
+            List<String> secondList = inputList.subList(headListSize + 2, inputList.size());
+            if (firstList.size() == headListSize && secondList.size() == tailListSize) {
+                Map<String, String> map = new LinkedHashMap<>();
+                List<String> outers = new ArrayList<>(secondList);
+                for (String string : firstList) {
+                    String result = secondList.stream()
+                            .filter(s -> StringUtils.containsIgnoreCase(s, string)
+                                    || StringUtils.containsIgnoreCase(string, s)
+                                    || hasSynonymWords(string, s)
+                                    || hasSameSubstringWithThisLength(s, string))
+                            .findAny()
+                            .orElse("?");
+                    map.put(string, result);
+                    outers.remove(result);
+                }
+                for (String s : outers) {
+                    map.put(s, "?");
+                }
+                return map;
+            } else {
+                log.info("There is mismatch of lengths from file with sizes of result arrays");
+                return null;
+            }
+        } else {
+            log.info("The program can't to find similar strings in empty sets");
+            return null;
         }
-        return map;
     }
 
     private static boolean hasSameSubstringWithThisLength(String s1, String s2) {
